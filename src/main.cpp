e0cc7e744a063bf973591698130247df5da5dc35
Erick Campos <erickcampos50@gmail.com>
2019-08-31 13:38:28 -0300

    Adicionado documentação via Doxygen

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,29 @@
+/**
+ * @file main.cpp
+ * @author Erick Campos (erickcampos50@gmail.com)
+ * @brief Controle de condicionadores de ar 
+ * @version 0.1
+ * @date 2019-08-31
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+
 #include <Arduino.h>
-//#include <WiFiClient.h>
+#include <WiFiClient.h>
 #include <WiFiManager.h>
 #include <ArduinoJson.h>
 #include <IRremoteESP8266.h>
 #include <aREST.h>
 
+/**
+ * @brief Função de configuração do código
+ * 
+ */
 void setup() {
   // put your setup code here, to run once:
 }
+
 
 void loop() {
   // put your main code here, to run repeatedly:
