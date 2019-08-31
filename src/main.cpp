cb75fd2a07e45627a34cf5c9dc6ba79b3f5ce1f8
Erick Campos <erickcampos50@gmail.com>
2019-08-31 11:55:58 -0300

    Inserindo dependencias e verificando sincronizacao

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
 #include <Arduino.h>
+//#include <WiFiClient.h>
+#include <WiFiManager.h>
+#include <ArduinoJson.h>
+#include <IRremoteESP8266.h>
+#include <aREST.h>
 
 void setup() {
   // put your setup code here, to run once:
